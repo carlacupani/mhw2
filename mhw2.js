130106831c6363c1001470ebd8cfa5b21c3b18bf
Carla Cupani <cpnclp00@gmail.com>
2024-04-10 01:27:48 +0200

    commit

--- mhw2.js
+++ mhw2.js
@@ -1,12 +1,8 @@
-
 const readMoreButtons = document.querySelectorAll('.faq li button');
 
 readMoreButtons.forEach(button => {
     button.addEventListener('click', () => {
-        
         const content = button.nextElementSibling;
-
-        
         if (content.style.display === 'block') {
             content.style.display = 'none';
             button.querySelector('img').style.transform = 'rotate(0deg)';
@@ -15,4 +11,16 @@ readMoreButtons.forEach(button => {
             button.querySelector('img').style.transform = 'rotate(45deg)';
         }
     });
+});
+
+var btnInviaEmail = document.getElementById("signup-btn");
+
+btnInviaEmail.addEventListener("click", function() {
+    var campoEmail = document.getElementById("signup-email");
+    var testoInserito = campoEmail.value;
+    
+    console.log("Testo dell'email:", testoInserito);
+
+    btnInviaEmail.classList.add("email-inviata");
+    btnInviaEmail.textContent = "Email Inviata!";
 });
